f726e587dde966857d0657606db8e561047b3ace
YourFriendlyEncounter <55287079+YourFriendlyEncounter@users.noreply.github.com>
2020-03-31 13:35:10 +0500

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,35 @@
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
+
+http.createServer((req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('Невозможно открыть файл "index.html".')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Сервер запущен на: http://localhost:%s', httpPort)
+})
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? ''
-    : '/'
+    : '/',
+    filenameHashing: false,
+    chainWebpack: config => {
+        config.module
+            .rule('svg')
+            .use('file-loader')
+            .options({
+                name: '[name].[ext]',
+                outputPath: ''
+            });
+    }
 }
